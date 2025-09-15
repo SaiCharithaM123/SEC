28543e61e9cc29b57fd6f60832f5b87ffbb66646
Sai Charitha <234g1a33d7@srit.ac.in>
2025-09-15 15:22:54 +0530

    commit

---
+++ my-counter-app/src/temp.jsx
@@ -0,0 +1,10 @@
+function welcome(prop){
+   return <h2>welcome (prop.name)!</h2>;
+}
+function temp(){
+    return(
+        <div>
+            <h2>"hi this is charitha"</h2>
+        </div>
+    )
+}
